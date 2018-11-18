80c8814fcecdca1a153fa20bcd1378102c64165a
Jay Chouhan <jaychouhan@Jays-MacBook-Pro.local>
2018-11-19 00:23:48 +0530

    ye lo api

--- routes/foo.js
+++ routes/foo.js
@@ -3,11 +3,46 @@ var router = express.Router()
 const userSchema = require('../schema/userSchema');
 const controller = require('../handlers/registrationHandler');
 
-
-// GET /foo
-// router.get('/', function (req, res, next) {
-//   res.send('this is the index for foo')
-// })
+/**
+ * @swagger
+ * definition:
+ *   users:
+ *     properties:
+ *        firstname:
+ *            type: string
+ *        surname:
+ *            type: string
+ *        mobile:
+ *            type: number
+ *        email:
+ *            type: string
+ *        dob:
+ *            type: string
+ *        gender:
+ *            type: string
+ *        password:
+ *            type: string
+ */
+/**
+ * @swagger
+ * /foo/products:
+ *   post:
+ *     tags:
+ *       - users
+ *     description: Creates a new user
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name:
+ *         description: user object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/user'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.route('/products').post(controller.insertdata)
 //=============Main
 /**
@@ -36,36 +71,5 @@ router.route('/products').post(controller.insertdata)
  *           $ref: '#/definitions/users'
  */
 router.route('/productsfind').get(controller.find)
-// router.route('/products').post(function (req, res) {
-//     console.log(  req.body.firstname)
-//      console.log( req.body.surname)
-//      console.log( req.body.mobile)
-//      console.log( req.body.email)
-//       console.log(req.body.dob)
-//       console.log(req.body.gender)
-//       console.log(req.body.password)
-     
-     
-//       userSchema.create({
-//           firstname:req.body.firstname,
-//           surname:req.body.surname,
-//           mobile:req.body.mobile,
-//           email:req.body.email,
-//           dob:req.body.dob,
-//           gender:req.body.gender,
-//           password:req.body.password
-//       },(err,doc)=>{
-//           if(err){
-//               throw err
-//           }
-//           if(doc){
-//               res.send({statuscode:200,doc,message:'Success'})
-//           }
-//           else{
-//               res.send({statuscode:201,message:'Fail'})
-//           }
-//       })
-//      //res.send({message:'Hii '+ req.body.username})
-//   });
 
 module.exports = router
